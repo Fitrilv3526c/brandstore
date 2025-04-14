1ceb3a665aa2c233661c010f9d1901c785085136
Fitrilv3526c <IwillBeBillionareOneDay@gmail.com>
2025-04-14 10:09:42 +0500

    01-create-next-app-with-shadcn

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}', // Adjust to match your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
