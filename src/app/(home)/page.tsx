c0d74dda1fc2a7985a4b152fe1849e92e8fd810b
Fitrilv3526c <IwillBeBillionareOneDay@gmail.com>
2025-04-14 15:47:14 +0500

    03-creating-a-carousal

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from '@/components/ui/button'
+import Carosal from '@/components/layout/carousal/Carosal'
 
 export default function Home() {
   return (
-    <div>
-      <Button>Shop Now</Button>
+    <div className=' '>
+      <div className=' mx-auto w-[80%] my-20'>
+        <Carosal />
+      </div>
     </div>
   )
 }
---
+++ src/components/layout/carousal/Carosal.tsx
@@ -0,0 +1,143 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import Image from 'next/image'
+
+const Carosal = () => {
+  return (
+    <Carousel>
+      <CarouselPrevious />
+      <CarouselContent>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client1.png'
+            alt='client-logo-1'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client2.png'
+            alt='client-logo-2'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client3.png'
+            alt='client-logo-3'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client4.png'
+            alt='client-logo-4'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client5.png'
+            alt='client-logo-5'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client1.png'
+            alt='client-logo-1'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client2.png'
+            alt='client-logo-2'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client3.png'
+            alt='client-logo-3'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client4.png'
+            alt='client-logo-4'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client5.png'
+            alt='client-logo-5'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client1.png'
+            alt='client-logo-1'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client2.png'
+            alt='client-logo-2'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client3.png'
+            alt='client-logo-3'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client4.png'
+            alt='client-logo-4'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+        <CarouselItem className='basis-1/5'>
+          <Image
+            src='/images/client5.png'
+            alt='client-logo-5'
+            width={150}
+            height={100}
+          />
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
+export default Carosal
